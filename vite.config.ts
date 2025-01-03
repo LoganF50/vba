5666f8e9a5f310dcd704a5573914a4211d3776c9
Logan Faschingbauer <faschingbauerl@yahoo.com>
2025-01-03 04:28:50 -0600

    add base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "https://loganf50.github.io/vba/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
